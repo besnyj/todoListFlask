3537f0425ab36d0a5cbb732b48eab23950cdff9d
Besnyj <verificacoesfelipe@gmail.com>
2023-11-24 06:51:06 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>homepage</h1>"
+
+@app.route("/about")
+def about():
+    return "about"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
